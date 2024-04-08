2000498928bc3d99cfa3b8db4702eb63f9481262
benefitsoul <166354472+benefitsoul@users.noreply.github.com>
2024-04-07 22:57:30 -0700

    Update article.js

--- src/api/article/services/article.js
+++ src/api/article/services/article.js
@@ -7,3 +7,11 @@
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::article.article');
+
+await strapi.plugins['email'].services.email.send({
+    to: 'benefitsoul@gmail.com',
+    from: 'no-reply@strapiapp.com', //e.g. single sender verification in SendGrid
+    subject: 'The Strapi Email plugin worked successfully',
+    text: 'Hello world!',
+    html: 'Hello world!',
+  }),
